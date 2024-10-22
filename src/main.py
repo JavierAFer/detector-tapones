1c1d635456392add868374e87da89179d7b2a003
Tu Nombre <javier.alvarofernandez99@gmail.com>
2024-10-22 12:50:52 +0200

    Añado la funcion main con toda la logica de uso de las funciones de  cargado y procesado de imagenes. Permitiendo elegir al usuario si el proceso se realiza para una imagen o para todas

--- src/main.py
+++ src/main.py
@@ -22,3 +22,37 @@ def procesar_imagen(gestor_mascaras, colores_seleccionados, colores_disponibles)
         # Combinar las máscaras para los colores seleccionados
         mascara_combinada = gestor_mascaras.combinar_mascaras(colores_seleccionados)
         gestor_mascaras.dibujar_contornos(mascara_combinada)
+
+
+def main():
+    colores_disponibles = mostrar_menu_colores()
+    color_elegido = input("Introduce los colores que deseas (separados por comas) o 'mascara completa': ").lower()
+    colores_seleccionados = [color.strip() for color in color_elegido.split(',')]
+
+    gestor_imagenes = GestorImagenes("./images")
+    imagenes = gestor_imagenes.cargar_y_copiar_imagenes()
+
+    if not imagenes:
+        print("No se encontraron imágenes.")
+        return
+
+    procesar_todas = input("¿Quieres procesar todas las imágenes? (si/no): ").lower()
+
+    if procesar_todas == "si":
+        for idx, imagen in enumerate(imagenes):
+            print(f"\nProcesando imagen {idx + 1}/{len(imagenes)}...")
+            gestor_mascaras = GestorMascaras(imagen)
+            procesar_imagen(gestor_mascaras, colores_seleccionados, colores_disponibles)
+    elif procesar_todas == "no":
+        imagen_idx = int(input(f"Introduce el número de la imagen que deseas procesar (1-{len(imagenes)}): ")) - 1
+        if 0 <= imagen_idx < len(imagenes):
+            imagen = imagenes[imagen_idx]
+            gestor_mascaras = GestorMascaras(imagen)
+            procesar_imagen(gestor_mascaras, colores_seleccionados, colores_disponibles)
+        else:
+            print("Número de imagen no válido.")
+    else:
+        print("Opción no válida.")
+
+if __name__ == "__main__":
+    main()
